8cdaf0f3d646d2a594022ae381ff99117c064c43
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-09 14:03:20 -0600

    stupid failure tweaks

--- scriptHelper.js
+++ scriptHelper.js
@@ -43,25 +43,25 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     } else {
         list.style.visibility = "visible";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-        copilotStatus.innerHTML = `Pilot ${copilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         if (fuelLevel < 10000 && cargoLevel > 10000) {
-            launchStatus.innerHTML = `Shuttle not ready for launch`;
-            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+            launchStatus.style.color = "rgb(65, 159, 106)";
             fuelStatus.innerHTML = `Fuel level too low for launch`;
             cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
         } else if (fuelLevel < 10000) {
-            launchStatus.innerHTML = `Shuttle not ready for launch`;
-            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+            launchStatus.style.color = "rgb(65, 159, 106)";
             fuelStatus.innerHTML = `Fuel level too low for launch`;
             //console.log(fuelStatus);
         } else if (cargoLevel > 10000) {
-            launchStatus.innerHTML = `Shuttle not ready for launch`;
-            launchStatus.style.color = "red";
+            launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+            launchStatus.style.color = "rgb(65, 159, 106)";
             cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
             //console.log(cargoStatus);
         } else {
             launchStatus.innerHTML = `Shuttle is Ready for Launch`;
-            launchStatus.style.color = "green";
+            launchStatus.style.color = "rgb(65, 159, 106)";
             fuelStatus.innerHTML = `Fuel level high enough for launch`;
             cargoStatus.innerHTML = `Cargo mass low enough for launch`;
             //console.log("ran4");
