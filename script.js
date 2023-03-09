bb55bfe1fdbba8183b1339e49ed837c3d4022545
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-08 19:25:28 -0600

    should be finished

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Write your JavaScript code here!
 
-const { myFetch, formSubmission } = require("./scriptHelper");
+// const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
+// const { myFetch, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
-
+    console.log("ran");
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
@@ -13,19 +15,26 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-
+       let planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
    })
 
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
     event.preventDefault();
-    let pilot = document.querySelector("input[name=pilotName]");
-    let copilot = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
+    let pilotInput = document.querySelector("input[name=pilotName]");
+    let pilot = pilotInput.value;
+    let copilotInput = document.querySelector("input[name=copilotName]");
+    let copilot = copilotInput.value;
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = fuelLevelInput.value;
+    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+    let cargoMass = cargoMassInput.value;
+    let list = document.getElementById("faultyItems");
 
-    formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoMass.value);
-
+    console.log("ran2");
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+    console.log("ran3");
    });
    
 });
