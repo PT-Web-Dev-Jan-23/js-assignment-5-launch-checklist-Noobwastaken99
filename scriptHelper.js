c2f04f05814006fe431bb0f48d90961908bf045a
Noobwastaken99 <czmoni99@yahoo.com>
2023-03-09 13:56:59 -0600

    fixed list visibility added to if statement

--- scriptHelper.js
+++ scriptHelper.js
@@ -41,25 +41,30 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number" ) {
         alert("Make sure to enter valid information for each field!");
     } else {
-        list.style.visability = "visible";
+        list.style.visibility = "visible";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Pilot ${copilot} is ready for launch`;
-        if (fuelLevel < 10000) {
+        if (fuelLevel < 10000 && cargoLevel > 10000) {
             launchStatus.innerHTML = `Shuttle not ready for launch`;
             launchStatus.style.color = "red";
             fuelStatus.innerHTML = `Fuel level too low for launch`;
-            console.log(fuelStatus);
+            cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+        } else if (fuelLevel < 10000) {
+            launchStatus.innerHTML = `Shuttle not ready for launch`;
+            launchStatus.style.color = "red";
+            fuelStatus.innerHTML = `Fuel level too low for launch`;
+            //console.log(fuelStatus);
         } else if (cargoLevel > 10000) {
             launchStatus.innerHTML = `Shuttle not ready for launch`;
             launchStatus.style.color = "red";
             cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
-            console.log(cargoStatus);
+            //console.log(cargoStatus);
         } else {
             launchStatus.innerHTML = `Shuttle is Ready for Launch`;
             launchStatus.style.color = "green";
             fuelStatus.innerHTML = `Fuel level high enough for launch`;
             cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-            console.log("ran4");
+            //console.log("ran4");
         }
     }
 }
